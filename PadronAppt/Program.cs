7a4812ed6fb1ea29fac7068d0a448c0889c03130
Hiran Desai <desaihi@ryanair.com>
2022-10-06 10:05:32 +0200

    Add project files.

---
+++ PadronAppt/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using PadronAppt;
+
+try
+{
+	var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+	
+	//Change this Id 129 which is for OAC Cuidad Lineal to what ever is appropriate for you.
+	await new PadronApp().Start(Convert.ToInt16(config["OfficeId"]),config["OfficeName"]);
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+}
+Console.ReadLine();
